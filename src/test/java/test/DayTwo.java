1bb5106f9d7f9e7ea03511691674dc86c936e1ea
Rakeshg6199 <rakeshkumar65281@gmail.com>
2024-02-16 23:51:45 +0530

    New

--- src/test/java/test/DayTwo.java
+++ src/test/java/test/DayTwo.java
@@ -5,6 +5,7 @@ public class DayTwo {
 	public static void main(String[] args) {
 		System.out.println("Jebasta paithiyam");
 		System.out.println("Rakesh Great boy");
+		System.out.println("Kumar");
 
 	}
 }
